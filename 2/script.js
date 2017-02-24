3386f36c28a340098bc397b5ca210e1669a17599
Tania Kotyk <kotik523@gmail.com>
2017-02-24 20:31:43 +0200

    vypravyla #2

--- 2/script.js
+++ 2/script.js
@@ -4,21 +4,18 @@ for (var i = 0; i<5; i++) {
      arrNames[i] = prompt('Введите имя для массива');
 }
 	
-for (i = 0; i < arrNames.length; i++) {
-	var userName = prompt('Введите имя пользователя');
-	
-     if (arrNames[i] == userName) {
-        alert (userName + ', Вы успешно вошли');
-        break;} 
+var userName = prompt('Введите имя пользователя');
+var res = 'Неверное имя пользователя';
 
-        else {
-        alert ('Неверное имя пользователя')}
- }      
+for (i = 0; i < arrNames.length; i++) {
+if (arrNames[i] == userName) {
+res = userName + ', Вы успешно вошли';
+break;} 
+}
+alert (res);
           
 
 
-
-     
 
        
  
